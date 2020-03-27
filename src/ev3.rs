9f92ddee49fa8807105d808102e6c29e59b7f3ab
Anonymous <anon@ymo.us>
2020-03-27 10:27:39 +0100

    remove fields in driver

--- src/ev3.rs
+++ src/ev3.rs
@@ -1,32 +1,20 @@
 use std::cell::RefCell;
 use std::rc::Rc;
 use std::fs::File;
-use std::collections::HashMap;
+use std::io::Error;
+use std::fs::{OpenOptions};
 
 #[derive(Clone)]
-pub struct Driver {
-    class_name: String,
-    name: String,
-    attributes: RefCell<HashMap<String, Attribute>>,
-}
+pub struct Driver { }
 
 impl Driver {
-    pub fn new() -> Driver {
-        Driver {
-            class_name: "a".to_owned(),
-            name: "a".to_owned(),
-            attributes: RefCell::new(HashMap::new()),
+    pub fn get_attribute(&self, attribute_name: &str) -> Attribute {
+        let f = Rc::new(RefCell::new(File::create("a").unwrap()));
+        Attribute {
+            file: f
         }
     }
-
-    pub fn get_attribute(&self, attribute_name: &str) -> Attribute {
-        let attributes = self.attributes.borrow_mut();
-        attributes.get(attribute_name).expect("Internal error in the attribute map").clone()
-    }
 }
-
-use std::io::Error;
-
 
 #[derive(Debug, Clone)]
 pub struct Attribute {
@@ -36,8 +24,6 @@ pub struct Attribute {
 pub trait Device {
     fn get_attribute(&self, name: &str) -> Attribute;
 }
-
-use std::fs::{OpenOptions};
 
 impl Attribute {
     pub fn new(class_name: &str, name: &str, attribute_name: &str) -> Result<Attribute, Error> {
