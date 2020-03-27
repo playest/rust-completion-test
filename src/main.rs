722f83d504a2075937faee450a22d464124c6172
Anonymous <anon@ymo.us>
2020-03-27 10:34:40 +0100

    one file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,32 @@
 mod ev3;
 
 use ev3dev_lang_rust_derive::{Device};
-use ev3::{Attribute, Device, Driver};
+
+use std::io::Error;
+
+#[derive(Clone)]
+pub struct Driver { }
+
+impl Driver {
+    pub fn get_attribute(&self, attribute_name: &str) -> Attribute {
+        Attribute { }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Attribute {
+}
+
+pub trait Device {
+    fn get_attribute(&self, name: &str) -> Attribute;
+}
+
+impl Attribute {
+    fn get_str(&self) -> Result<String, Error> {
+        Ok("a".to_owned())
+    }
+}
+
 
 #[derive(Clone, Device)]
 struct Thingy {
